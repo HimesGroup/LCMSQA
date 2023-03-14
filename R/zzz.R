7571c989fa166a5f65c7c54da7daec89cf1b19fc
Jaehyun Joo <jaehyunjoo@outlook.com>
2023-03-14 11:20:42 -0400

    Added vignette

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(...){
   suppressPackageStartupMessages(require("MSnbase", quietly = TRUE))
+  suppressPackageStartupMessages(require("BiocParallel", quietly = TRUE))
   packageStartupMessage("Welcome to the LCMSQA package!")
 }
